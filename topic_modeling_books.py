947d96a61cd9f2a8b74cab502beef035e0b994ea
diana-esaian <90722094+diana-esaian@users.noreply.github.com>
2023-05-21 14:56:08 +0400

    Update topic_modeling_books.py
    instead of gensim used sklearn

--- topic_modeling_books.py
+++ topic_modeling_books.py
@@ -1,13 +1,6 @@
 import os
-# os.system("pip install gensim")
-# os.system("pip install natasha")
-# os.system("pip install pymorphy2")
-# os.system("pip install nltk")
-# import nltk
-from nltk.tokenize import word_tokenize 
-from nltk import download as nltk_download 
-# nltk_download ('punkt')
-# import natasha
+os.system("pip install natasha")
+os.system("pip install scikit-learn")
 from natasha import (Segmenter, MorphVocab, NewsEmbedding, NewsMorphTagger, NewsSyntaxParser, NewsNERTagger, Doc)
 segmenter = Segmenter()
 morph_vocab = MorphVocab()
@@ -15,109 +8,93 @@ emb = NewsEmbedding()
 morph_tagger = NewsMorphTagger(emb)
 syntax_parser = NewsSyntaxParser(emb)
 ner_tagger = NewsNERTagger(emb)
-# import pymorphy2
-from pymorphy2 import MorphAnalyzer
-parser = MorphAnalyzer()
-# gensim
-import gensim
-# import re
 import re
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 
-def main():
-    # text_folder and stop_folder
-    text_folder = 'texts'
-    texts = os.listdir(text_folder)
-    stop_folder = 'stop'
-    stop = os.listdir(stop_folder)
+text_folder = 'texts'
+texts = os.listdir(text_folder)
 
-    # lists for future dataset
-    authors = []
-    books = []
+# lists for future dataset
+all_authors = []
+all_books = []
+all_topics = []
+
+def preprocess(input_text, punctuation, stop_words):
+  doc_natasha = Doc(input_text)
+  doc_natasha.segment(segmenter)
+  doc_natasha.tag_morph(morph_tagger)
+  doc_natasha.parse_syntax(syntax_parser)
+  doc_natasha.tag_ner(ner_tagger)
+  allowed_pos = ['NOUN', 'ADJ', 'VERB', 'ADV']
+  lemmatized_text = []
+  names = []
+  for span in doc_natasha.spans:
+    if span.type == 'PER':
+      span.normalize(morph_vocab)
+      if span.normal not in names:
+        names.append(span.normal.lower())
+  black_list = stop_words + list(punctuation)
+  for name in names:
+    word = name.lower()
+    black_list = black_list + word.split(' ')
+  for token in doc_natasha.tokens:
+    if token.pos in allowed_pos:
+      token.lemmatize(morph_vocab)
+      if token.lemma not in black_list:
+        if re.findall('\D+', token.lemma) != 0:
+          lemmatized_text.append(token.lemma)
+  return lemmatized_text
+
+with open ('stop_ru.txt', encoding="utf8") as stop_file:
+    rus_stops = [word.strip() for word in stop_file.readlines()]
+
+punctuation = '!\"#$%&\'()*+,…-./' + ":;<=>?@[\]^_`{|}~—»«...–"
+
+count = 1
+for text in texts:
+    with open(os.path.join(text_folder, text), encoding="utf-8") as f:
+        text = f.read()
+    lemmatized_text = preprocess(text, punctuation, rus_stops)
+    cv = TfidfVectorizer(lowercase=True)
+    dtm = cv.fit_transform(lemmatized_text)
+    model=LatentDirichletAllocation(n_components=5)
+    model.fit(dtm)
+    lda_matrix = model.fit_transform(dtm)
+    lda_components=model.components_
+    terms = cv.get_feature_names_out()
     topics = []
+    for index, component in enumerate(lda_components):
+        zipped = zip(terms, component)
+        top_terms_key=sorted(zipped, key = lambda t: t[1], reverse=True)[:7]
+        top_terms_list=list(dict(top_terms_key).keys())
+        # print("Topic "+str(index)+": ",top_terms_list)
+        topics.append(top_terms_list)
+   
+    # index_author = text.index('.')
+    # author = text[:index_author]
+    # print(author)
+    # all_authors.append(author)
 
-    # initial black list
-    rus_stops = []
-    for st in stop:
-        with open(os.path.join(stop_folder, st), encoding="utf-8") as f:
-            rus_st = [word.strip() for word in f.readlines()]
-            rus_stops = rus_stops + rus_st
+    # extract book title 
+    # index_title = text.rindex('.')
+    # title = text[index_author+1:index_title]
+    # all_books.append(title)
+    # print(index_title)
 
-    punctuation = '!\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~—»«...–'
+    # topics
+    all_topics.append(topics)
+    print(topics)
 
-    # # function for preprocessing 
-    # def preprocess(input_text):
-    #     tokenized_text = word_tokenize(input_text.lower())
-    #     output_text = [word for word in tokenized_text if word not in filters]
-    #     clean_text = [word for word in output_text if word.isalpha()]
-    #     lemmatized_text = [parser.parse(word)[0].normal_form for word in clean_text]
-    #     return lemmatized_text
+    print(count)
+    count +=1
 
-    # read every book
-    for book in texts:
-        with open(os.path.join(text_folder, book), encoding="utf-8") as f:
-            text = f.read()
-        text_list = []
-        text_list.append(text)
-        # append black list
-        doc_natasha = Doc(text)
-        doc_natasha.segment(segmenter)
-        doc_natasha.tag_morph(morph_tagger)
-        doc_natasha.parse_syntax(syntax_parser)
-        doc_natasha.tag_ner(ner_tagger)
-        names = []
-        for span in doc_natasha.spans:
-            if span.type == 'PER':
-                if span.text not in names:
-                    names.append(span.text)
-                span.normalize(morph_vocab)
-                if span.normal not in names:
-                    names.append(span.normal)
-        filters = rus_stops + list(punctuation)
-        for name in names:
-            word = name.lower()
-            filters = filters + word.split(' ')
-        # extract topics
-        def preprocess(input_text):
-            tokenized_text = word_tokenize(input_text.lower())
-            output_text = [word for word in tokenized_text if word not in filters]
-            clean_text = [word for word in output_text if word.isalpha()]
-            lemmatized_text = [parser.parse(word)[0].normal_form for word in clean_text]
-            return lemmatized_text
-        preprocessed_texts = [preprocess(text_t) for text_t in text_list]
-        gensim_dictionary_for_TM = gensim.corpora.Dictionary(preprocessed_texts)
-        gensim_dictionary_for_TM.compactify()
-        corpus = [gensim_dictionary_for_TM.doc2bow(text_p) for text_p in preprocessed_texts]
-        lda = gensim.models.LdaMulticore(corpus,num_topics = 10, id2word=gensim_dictionary_for_TM, passes=10)
-        topics_book = []
-        for topic in lda.print_topics():
-            topic_extracted = re.findall(r'(?<=")\w.*?(?=")', topic[1])
-            for i in topic_extracted:
-                if i not in topics_book:
-                    topics_book.append(i)
+df_columns = ['author', 'book', 'topics']
+dataframe = pd.DataFrame(columns = df_columns)
+# dataframe['author'] = all_authors
+# dataframe['book'] = all_books
+dataframe['topics'] = all_topics
 
-        # extract author's name
-        index_author = text.index('.')
-        author = text[:index_author]
-        authors.append(author)
-
-        # extract book title 
-        index_title = text.rindex('.')
-        title = text[index_author+1:index_title]
-        books.append(title)
-
-        # topics 
-        topics.append(topics_book)
-
-    df_columns = ['author', 'book', 'topics']
-    dataframe = pd.DataFrame(columns = df_columns)
-    dataframe['author'] = authors
-    dataframe['book'] = books
-    dataframe['topics'] = topics
-
-    # creating a dataset 
-    dataframe.to_csv('topics_books.csv')
-
-
-
-if __name__ == '__main__':
-    main()
+# creating a dataset 
+dataframe.to_csv('topics_books.csv')
